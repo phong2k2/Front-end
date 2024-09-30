0ec518a3dad5ffd9069b7e458da9403459023888
phong2k2 <lnbphong.20it2@vku.udn.vn>
2024-09-30 15:42:10 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slides/cuonterSlide'
+import styled from 'styled-components'
+
+function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  const Button = styled.button({
+    background: 'red',
+  });
+
+  return (
+    <div>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
